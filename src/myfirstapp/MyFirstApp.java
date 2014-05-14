2b1ba400b05a6b02e0534ff06901621d8e82e265
tonglikeanime@hotmail.co.th <>
2014-05-14 09:14:25 +0700

    สร้างโปรเจ็ค2ลืมแอดโปรเจ็ค

---
+++ src/myfirstapp/MyFirstApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myfirstapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MyFirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("55024400");
+        System.out.println("Kantapong Rakpong");
+       
+    }
+    
+}
